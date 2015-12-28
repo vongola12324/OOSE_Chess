6bebb73d3fdf1730bcd7b5285194d1eef11fbb24
Ben Tisa <powdersave@gmailc.om>
2015-12-28 22:51:48 +0800

    五子棋rule重寫,親測可用

--- src/Rule.java
+++ src/Rule.java
@@ -25,110 +25,124 @@ class GomokuRule extends Rule {
     // Fixme: Change to short and return who win
     @Override
     public short check(final Chess[][] ChessStatus) {
-        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length;
-        int if_fullChess = 0;
+        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length, i, j;
+        short CheckResault = Const.NO_WIN;
+        Chess CheckChess;
+        for (i = 0; i < Row_len; i++) {
+            for (j = 0; j < Col_len; j++) {
+                CheckChess = ChessStatus[i][j];
+                if (ChessStatus[i][j] != null) {
+                    if (CheckResault == Const.NO_WIN) {
+                        CheckResault = LeftLastCheck(ChessStatus, CheckChess);
+                    }
+                    if (CheckResault == Const.NO_WIN) {
+                        CheckResault = DownLastCheck(ChessStatus, CheckChess);
+                    }
+                    if (CheckResault == Const.NO_WIN) {
+                        CheckResault = LeftDownLastCheck(ChessStatus, CheckChess);
+                    }
+                    if (CheckResault == Const.NO_WIN) {
+                        CheckResault = LeftUpLastCheck(ChessStatus, CheckChess);
+                    }
+                    if (CheckResault == Const.NO_WIN) {
+                        CheckResault = TieCheck(ChessStatus, CheckChess);
+                    }
+                }
+            }
+        }
+        if (CheckResault != Const.NO_WIN) {
+            return CheckResault;
+        }
+        return Const.NO_WIN;
+    }
 
-//        for (int i = 0; i < Row_len; i++) {
-//            for (int j = i; j < Col_len; j++) {
-//                if (ChessStatus[i][j] != null) {
-//                    int CheckPoint = i + 1, CheckLocX = 1, CheckLocY = 1;
-//                    boolean CheckResault = false;
-//                    short nowColor = ChessStatus[i][j].getColor();
-//
-//                    //Scan Left 4 chess
-//                    while (CheckPoint < Col_len && CheckPoint < j + 4) {
-//                        if (ChessStatus[i][CheckPoint] != null &&
-//                                ChessStatus[i][CheckPoint].getColor() != nowColor) {
-//                            CheckResault = false;
-//                            break;
-//                        } else {
-//                            CheckResault = true;
-//                        }
-//                        CheckPoint++;
-//                    }
-//                    if (CheckResault == true) {//for leftscan resault
-//                        if (nowColor == Const.WHITE_CHESS)
-//                            return Const.WHITE_WIN;
-//                        else
-//                            return Const.BLACK_WIN;
-//                    }
-//
-//                    //Scan Down 4 chess
-//                    CheckPoint = i + 1;
-//                    while (CheckPoint + 1 < Row_len && CheckPoint < i + 4) {
-//                        if (ChessStatus[CheckPoint][j] != null &&
-//                                ChessStatus[CheckPoint][j].getColor() != nowColor) {
-//                            CheckResault = false;
-//                            break;
-//                        } else {
-//                            CheckResault = true;
-//                        }
-//                        CheckPoint++;
-//                    }
-//                    if (CheckResault == true) {//for downscan resault
-//                        if (nowColor == Const.WHITE_CHESS)
-//                            return Const.WHITE_WIN;
-//                        else
-//                            return Const.BLACK_WIN;
-//                    }
-//
-//                    //Scan LeftUp 4 chess
-//                    CheckLocX = 1;
-//                    CheckLocY = 1;
-//                    while (CheckLocX + i < Row_len && CheckLocY + j < CheckLocY) {
-//                        if (ChessStatus[CheckLocX + i][CheckLocY + j] != null &&
-//                                ChessStatus[CheckLocX + i][CheckLocY + j].getColor() != nowColor) {
-//                            CheckResault = false;
-//                            break;
-//                        } else {
-//                            CheckResault = true;
-//                        }
-//                        if (CheckLocX < 5) {
-//                            CheckLocX++;
-//                            CheckLocY++;
-//                        }
-//                    }
-//                    if (CheckResault == true) {//for leftupScan resault
-//                        if (nowColor == Const.WHITE_CHESS)
-//                            return Const.WHITE_WIN;
-//                        else
-//                            return Const.BLACK_WIN;
-//                    }
-//                    CheckLocX = 1;
-//                    CheckLocY = 1;
-//
-//                    //Scan LeftDown 4 chess
-//                    while (CheckLocX + i < Row_len && CheckLocY + j < CheckLocY) {
-//                        if (ChessStatus[CheckLocX + i][j - CheckLocY] != null &&
-//                                ChessStatus[CheckLocX + i][j - CheckLocY].getColor() != nowColor) {
-//                            CheckResault = false;
-//                            break;
-//                        } else {
-//                            CheckResault = true;
-//                        }
-//                        if (CheckLocX < 5) {
-//                            CheckLocX++;
-//                            CheckLocY++;
-//                        }
-//                    }//
-//                    if (CheckResault == true) {//for leftdownScan resault
-//                        if (nowColor == Const.WHITE_CHESS)
-//                            return Const.WHITE_WIN;
-//                        else
-//                            return Const.BLACK_WIN;
-//                    } else {
-//                        if (i + 1 == Row_len && j + 1 == Col_len) {//Check if Tie
-//                            if (if_fullChess == 0) {
-//                                return Const.TIE;
-//                            }
-//                        }
-//                        return Const.NO_WIN;
-//                    }
-//                } else {
-//                    if_fullChess = 1;//To Check if ChessBoard Is Full
-//                }
-//            }
-//        }
+    public short DownLastCheck(final Chess[][] ChessStatus, Chess CheckChess) {
+        int Row_len = ChessStatus.length, LocX = CheckChess.getNowLocX(), LocY = CheckChess.getNowLocY();
+        int CheckPoint = LocY;
+        int CheckResault = 0;
+        while (CheckPoint < Row_len && CheckPoint < LocY + 6 && ChessStatus[LocX][CheckPoint] != null) {
+            if (CheckChess.color == ChessStatus[LocX][CheckPoint].color) {
+                CheckResault++;
+            }
+            CheckPoint++;
+        }
+        if (CheckResault >= 5) {
+            if (CheckChess.color == Const.WHITE_CHESS) {
+                return Const.WHITE_WIN;
+            } else {
+                return Const.BLACK_WIN;
+            }
+        }
+        return Const.NO_WIN;
+    }
+
+    public short LeftLastCheck(final Chess[][] ChessStatus, Chess CheckChess) {
+        int Col_len = ChessStatus[0].length, LocX = CheckChess.getNowLocX(), LocY = CheckChess.getNowLocY();
+        int CheckPoint = LocX;
+        int CheckResault = 0;
+        while (CheckPoint < Col_len && CheckPoint < LocX + 6 && ChessStatus[CheckPoint][LocY] != null) {
+            if (CheckChess.color == ChessStatus[CheckPoint][LocY].color) {
+                CheckResault++;
+            }
+            CheckPoint++;
+        }
+        if (CheckResault >= 5) {
+            if (CheckChess.color == Const.WHITE_CHESS) {
+                return Const.WHITE_WIN;
+            } else {
+                return Const.BLACK_WIN;
+            }
+        }
+        return Const.NO_WIN;
+    }
+
+    public short LeftDownLastCheck(final Chess[][] ChessStatus, Chess CheckChess) {
+        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length, LocX = CheckChess.getNowLocX(), LocY = CheckChess.getNowLocY();
+        int CheckPointX = LocX, CheckPointY = LocY;
+        int CheckResault = 0;
+        while (CheckPointX < Col_len && CheckPointY < Row_len && CheckPointX < LocX + 6 && CheckPointY < LocY + 6 && ChessStatus[CheckPointX][CheckPointY] != null) {
+            if (CheckChess.color == ChessStatus[CheckPointX][CheckPointY].color) {
+                CheckResault++;
+            }
+            CheckPointX++;
+            CheckPointY++;
+        }
+        if (CheckResault >= 5) {
+            if (CheckChess.color == Const.WHITE_CHESS) {
+                return Const.WHITE_WIN;
+            } else {
+                return Const.BLACK_WIN;
+            }
+        }
+        return Const.NO_WIN;
+    }
+
+    public short LeftUpLastCheck(final Chess[][] ChessStatus, Chess CheckChess) {
+        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length, LocX = CheckChess.getNowLocX(), LocY = CheckChess.getNowLocY();
+        int CheckPointX = LocX, CheckPointY = LocY;
+        int CheckResault = 0;
+        while (CheckPointX < Col_len && CheckPointY < Row_len && CheckPointX < LocX + 6 && CheckPointY < LocY + 6 && ChessStatus[CheckPointX][CheckPointY] != null) {
+            if (CheckChess.color == ChessStatus[CheckPointX][CheckPointY].color) {
+                CheckResault++;
+            }
+            CheckPointX++;
+            CheckPointY--;
+        }
+        if (CheckResault >= 5) {
+            if (CheckChess.color == Const.WHITE_CHESS) {
+                return Const.WHITE_WIN;
+            } else {
+                return Const.BLACK_WIN;
+            }
+        }
+        return Const.NO_WIN;
+    }
+
+    public short TieCheck(final Chess[][] ChessStatus, Chess CheckChess) {
+        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length, LocX = CheckChess.getNowLocX(), LocY = CheckChess.getNowLocY();
+        if (Row_len == LocX && Col_len == LocY) {
+            return Const.TIE;
+        }
         return Const.NO_WIN;
     }
 }
