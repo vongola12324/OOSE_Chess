de11fcce07f38733dbac5f61821c749501556626
Kevin <ds934118@gmail.com>
2015-12-28 10:47:00 +0800

    add update Chess

--- src/Chess.java
+++ src/Chess.java
@@ -4,55 +4,63 @@ public class Chess {
     short color;
     Integer step;
 
-    public Chess(short color, Location loc, int step){
+    public Chess(short color, Location loc, int step) {
         this.setColor(color);
         this.setNowLoc(loc);
     }
 
-    public void setNowLoc(Location loc){
+    public void setNowLoc(Location loc) {
         this.nowLoc = new Location(loc);
     }
 
-    public Location getNowLoc(){
+    public Location getNowLoc() {
         return new Location(this.nowLoc);
     }
 
-    public void setColor(short color){
+    public int getNowLocX() {
+        return this.nowLoc.getX();
+    }
+
+    public int getNowLocY() {
+        return this.nowLoc.getY();
+    }
+
+    public void setColor(short color) {
         this.color = color;
     }
 
-    public short getColor(){
+    public short getColor() {
         return this.color;
     }
 
-    public void setStep(int step){
+    public void setStep(int step) {
         this.step = step;
     }
 
-    public Integer getStep(){
+    public Integer getStep() {
         return this.step;
     }
 
 }
 
-class Location{
+class Location {
     private Integer x, y;
 
-    public Location(Integer x, Integer y){
+    public Location(Integer x, Integer y) {
         this.x = new Integer(x);
         this.y = new Integer(y);
     }
 
-    public Location(Location loc){
+    public Location(Location loc) {
         this.x = loc.getX();
         this.y = loc.getY();
     }
 
-    public Integer getX(){
+    public Integer getX() {
         return this.x;
     }
 
-    public Integer getY(){
+    public Integer getY() {
         return this.y;
     }
 
@@ -66,13 +74,13 @@ class Location{
 
 
     @Override
-    public String toString(){
+    public String toString() {
         return "(" + this.getX() + ", " + this.getY() + ")";
     }
 }
 
-class ChessFactory{
-    public Chess makeChess(short color, Location loc, int step){
+class ChessFactory {
+    public Chess makeChess(short color, Location loc, int step) {
         return new Chess(color, loc, step);
     }
 
