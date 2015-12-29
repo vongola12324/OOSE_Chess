1cd1a56e0edbf8663fddef0ff9e31b408c9f0d0c
Ben Tisa <powdersave@gmailc.om>
2015-12-29 10:07:01 +0800

    Eatable Rule 版本一,未測試

--- src/Rule.java
+++ src/Rule.java
@@ -20,16 +20,16 @@ class GoRule extends Rule implements Eatable {
     @Override
     public void eat(Chess[][] ChessStatus) {
         // TODO: Eat Dot
-        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length, i, j, CheckLocX, CheckLocY;
-        int[][] Mapping = new int[Row_len][Col_len], CanBeEat = new int[Row_len][Col_len], Stack = new int[Row_len][Col_len];
-        int[][][] CheckPoint = new int[Row_len][Col_len][4];
+        int Row_len = ChessStatus.length, Col_len = ChessStatus[0].length, i, j, CheckLocX, CheckLocY, Mark = 0;
+        int[][] Mapping = new int[Row_len][Col_len], CanBeEat = new int[Row_len][Col_len], HasGone = new int[Row_len][Col_len];
+        int[][][] CheckPoint = new int[Row_len][Col_len][4], Stack = new int[Row_len][Col_len][2], CanGo = new int[Row_len][Col_len][4];
         //CheckPoint Comment
         //Up 0 -> Left 1 -> Down 2 -> Right 3
         //NotThing 0 , Same Color 1, Diff Color 2, NeverGo 3
 
         boolean EatAble = false;
 
-        ArrayIniter(ChessStatus, Mapping, CanBeEat, Stack, CheckPoint, Row_len, Col_len);//init array
+        ArrayIniter(ChessStatus, Mapping, CanBeEat, HasGone, Stack, CheckPoint, CanGo, Row_len, Col_len);//init array
 
         //Mapping
         for (i = 0; i < Row_len; i++) {
@@ -37,58 +37,126 @@ class GoRule extends Rule implements Eatable {
                 CheckLocX = i;
                 CheckLocY = j;
                 if (Mapping[i][j] != 0) {
-                    //==================================================================================//
-                    //CheckPoint Begin//
-                    if (i + 1 > Row_len) {//check if has wall
-                        CheckPoint[i][j][1] = 2;
-                    } else if (i - 1 < 0) {
-                        CheckPoint[i][j][4] = 2;
-                    } else if (j + 1 > Col_len) {
-                        CheckPoint[i][j][0] = 2;
-                    } else if (j - 1 < 0) {
-                        CheckPoint[i][j][3] = 2;
-                        /////////////////////////////////////////////////////////////////////////
-                    } else if (Mapping[i][j] != Mapping[i][j + 1] && Mapping[i][j + 1] != 0) {//check if Diff Color
-                        CheckPoint[i][j][0] = 2;
-                    } else if (Mapping[i][j] != Mapping[i + 1][j] && Mapping[i + 1][j] != 0) {
-                        CheckPoint[i][j][1] = 2;
-                    } else if (Mapping[i][j] != Mapping[i][j - 1] && Mapping[i][j - 1] != 0) {
-                        CheckPoint[i][j][2] = 2;
-                    } else if (Mapping[i][j] != Mapping[i - 1][j] && Mapping[i - 1][j] != 0) {
-                        CheckPoint[i][j][3] = 2;
-                        //////////////////////////////////////////////////////////////////////////
-                    } else if (Mapping[i][j] == Mapping[i][j + 1]) {//check if Same Color
-                        CheckPoint[i][j][0] = 1;
-                    } else if (Mapping[i][j] != Mapping[i + 1][j]) {
-                        CheckPoint[i][j][1] = 1;
-                    } else if (Mapping[i][j] != Mapping[i][j - 1]) {
-                        CheckPoint[i][j][2] = 1;
-                    } else if (Mapping[i][j] != Mapping[i - 1][j]) {
-                        CheckPoint[i][j][3] = 1;
-                        ///////////////////////////////////////////////////////////////////////////
-                    } else if (Mapping[i][j + 1] == 0) {//check if Nothing here
-                        CheckPoint[i][j][0] = 0;
-                        EatAble = false;
-                    } else if (Mapping[i + 1][j] == 0) {
-                        CheckPoint[i][j][1] = 0;
-                        EatAble = false;
-                    } else if (Mapping[i][j - 1] == 0) {
-                        CheckPoint[i][j][2] = 0;
-                        EatAble = false;
-                    } else if (Mapping[i - 1][j] == 0) {
-                        CheckPoint[i][j][3] = 0;
-                        EatAble = false;
-                        ///////////////////////////////////////////////////////////////////////////
+
+                    Stack[i][j][0] = -2;
+                    Stack[i][j][1] = -2;//-2 is start positon Stack[i][j][0] is LocX  Stack[i][j][1] is LocY
+                    HasGone[i][j] = 1;
+                    while (Stack[i][j][0] == -1 && Stack[i][j][1] == -1) {
+                        Mark = Checker(CheckPoint, Mapping, CheckLocX, CheckLocY);//check
+                        if (Mark == 1)//if check Nothing
+                            break;
+                        //if checked, go next, and stack position
+                        //first, search where can go
+                        for (int CheckCanGo = 0; CheckCanGo < 4; CheckCanGo++) {//check where can go
+                            if (CheckCanGo == 0) {
+                                if (CheckPoint[CheckLocX][CheckLocY][CheckCanGo] == 1 && HasGone[CheckLocX][CheckLocY + 1] == 0)
+                                    CanGo[CheckLocX][CheckLocY][CheckCanGo] = 1;
+                            }
+                            if (CheckCanGo == 1) {
+                                if (CheckPoint[CheckLocX][CheckLocY][CheckCanGo] == 1 && HasGone[CheckLocX + 1][CheckLocY] == 0)
+                                    CanGo[CheckLocX][CheckLocY][CheckCanGo] = 1;
+                            }
+                            if (CheckCanGo == 2) {
+                                if (CheckPoint[CheckLocX][CheckLocY][CheckCanGo] == 1 && HasGone[CheckLocX][CheckLocY - 1] == 0)
+                                    CanGo[CheckLocX][CheckLocY][CheckCanGo] = 1;
+                            }
+                            if (CheckCanGo == 3) {
+                                if (CheckPoint[CheckLocX][CheckLocY][CheckCanGo] == 1 && HasGone[CheckLocX - 1][CheckLocY] == 0)
+                                    CanGo[CheckLocX][CheckLocY][CheckCanGo] = 1;
+                            }
+                        }
+
+                        for (int Way_to_go = 0; Way_to_go < 4; Way_to_go++) {//goto next point
+                            if (CheckPoint[CheckLocX][CheckLocY][Way_to_go] == 1) {//if same color
+                                if (Way_to_go == 0) {
+                                    CheckLocY++;//move
+                                    if (HasGone[CheckLocX][CheckLocY] == 0) {// if never gone
+                                        HasGone[CheckLocX][CheckLocY] = 1;//now is gone
+                                        CanGo[CheckLocX][CheckLocY - 1][Way_to_go] = 0;//now cannot go
+                                        CanBeEat[CheckLocX][CheckLocY - 1] = 1;//Mark Eatable Buff
+                                        Stack[CheckLocX][CheckLocY][0] = CheckLocX;//Stack LocX Record
+                                        Stack[CheckLocX][CheckLocY][1] = CheckLocY - 1;//Stack LocY Record
+                                        break;//don't do others
+                                    } else
+                                        CheckLocY--;//return move back
+                                } else if (Way_to_go == 1) {
+                                    CheckLocX++;
+                                    if (HasGone[CheckLocX][CheckLocY] == 0) {
+                                        HasGone[CheckLocX][CheckLocY] = 1;
+                                        CanGo[CheckLocX - 1][CheckLocY][Way_to_go] = 0;
+                                        CanBeEat[CheckLocX - 1][CheckLocY] = 1;
+                                        Stack[CheckLocX][CheckLocY][0] = CheckLocX - 1;
+                                        Stack[CheckLocX][CheckLocY][1] = CheckLocY;
+                                        break;
+                                    } else
+                                        CheckLocX++;
+                                } else if (Way_to_go == 2) {
+                                    CheckLocY--;
+                                    if (HasGone[CheckLocX][CheckLocY] == 0) {
+                                        HasGone[CheckLocX][CheckLocY] = 1;
+                                        CanGo[CheckLocX][CheckLocY + 1][Way_to_go] = 0;
+                                        CanBeEat[CheckLocX][CheckLocY + 1] = 1;
+                                        Stack[CheckLocX][CheckLocY][0] = CheckLocX;
+                                        Stack[CheckLocX][CheckLocY][1] = CheckLocY + 1;
+                                        break;
+                                    } else
+                                        CheckLocY++;
+                                } else {
+                                    CheckLocX--;
+                                    if (HasGone[CheckLocX][CheckLocY] == 0) {
+                                        HasGone[CheckLocX][CheckLocY] = 1;
+                                        CanGo[CheckLocX + 1][CheckLocY][Way_to_go] = 0;
+                                        CanBeEat[CheckLocX + 1][CheckLocY] = 1;
+                                        Stack[CheckLocX][CheckLocY][0] = CheckLocX + 1;
+                                        Stack[CheckLocX][CheckLocY][1] = CheckLocY;
+                                        break;
+                                    } else
+                                        CheckLocX--;
+                                }
+                            }
+                        }//first step end
+
+                        //Second, if no where to go, back to nearst position which  has CanGo == 1
+                        if (CanGo[CheckLocX][CheckLocY][0] == 0 || CanGo[CheckLocX][CheckLocY][1] == 0
+                                || CanGo[CheckLocX][CheckLocY][2] == 0 || CanGo[CheckLocX][CheckLocY][3] == 0) {
+
+                            while (CanGo[CheckLocX][CheckLocY][0] == 0 || CanGo[CheckLocX][CheckLocY][1] == 0
+                                    || CanGo[CheckLocX][CheckLocY][2] == 0 || CanGo[CheckLocX][CheckLocY][3] == 0) {
+                                int StackLocX = Stack[CheckLocX][CheckLocY][0], StackLocY = Stack[CheckLocX][CheckLocY][1];
+                                Stack[CheckLocX][CheckLocY][0] = -1;
+                                Stack[CheckLocX][CheckLocY][1] = -1;
+                                CheckLocX = StackLocX;
+                                CheckLocY = StackLocY;
+                            }
+                        }
+                    }//While loop end
+                    if (Mark == 0) {//check where can be eat
+                        EatAble = true;
+                    } else {// nothing to  eat
+                        ArrayIniter(ChessStatus, Mapping, CanBeEat, HasGone, Stack, CheckPoint, CanGo, Row_len, Col_len);//init array
                     }
-                    //CheckPoint End//
-                    //==================================================================================//
-                }
-            }
-        }
+                    if (EatAble == true) {//Eating
+                        Eating(ChessStatus, CanBeEat, Row_len, Col_len);
+                        ArrayIniter(ChessStatus, Mapping, CanBeEat, HasGone, Stack, CheckPoint, CanGo, Row_len, Col_len);//init array
+                        EatAble = false;
+                    }
+                }//if has chess
+            }//for j loop
+        }// for i loop
 
     }
 
-    public static void ArrayIniter(Chess[][] ChessStatus, int[][] Mapping, int[][] CanBeEat, int[][] Stack, int[][][] CheckPoint, int Row_len, int Col_len) {
+    public static void Eating(Chess[][] ChessStatus, int[][] CanBeEat, int Row_len, int Col_len) {
+        for (int i = 0; i < Row_len; i++) {
+            for (int j = 0; j < Col_len; j++) {
+                if (CanBeEat[i][j] == 1) {
+                    ChessStatus[i][j] = null;
+                }
+            }
+        }
+    }
+
+    public static void ArrayIniter(Chess[][] ChessStatus, int[][] Mapping, int[][] CanBeEat, int[][] HasGone, int[][][] Stack, int[][][] CheckPoint, int[][][] CanGo, int Row_len, int Col_len) {
         for (int i = 0; i < Row_len; i++) {
             for (int j = 0; j < Col_len; j++) {
                 if (ChessStatus[i][j] == null) {
@@ -96,17 +164,68 @@ class GoRule extends Rule implements Eatable {
                 } else {
                     Mapping[i][j] = ChessStatus[i][j].color;
                 }
+                HasGone[i][j] = 0;
                 CanBeEat[i][j] = 0;
-                Stack[i][j] = 0;
+                Stack[i][j][0] = -1;
+                Stack[i][j][1] = -1;
+                CanGo[i][j][0] = 0;
+                CanGo[i][j][1] = 0;
+                CanGo[i][j][2] = 0;
+                CanGo[i][j][3] = 0;
+                CheckPoint[i][j][0] = 3;
+                CheckPoint[i][j][1] = 3;
+                CheckPoint[i][j][2] = 3;
+                CheckPoint[i][j][3] = 3;
             }
         }
-        for (int[][] elementI : CheckPoint) {
-            for (int[] elementJ : elementI) {
-                for (int elementK : elementJ) {
-                    elementK = 3;
-                }
-            }
+    }
+
+    public static short Checker(int[][][] CheckPoint, int[][] Mapping, int i, int j) {
+        int Row_len = Mapping.length, Col_len = Mapping[0].length;
+        short Mark = 0;
+
+        if (i + 1 > Row_len) {//check if has wall
+            CheckPoint[i][j][1] = 2;
+        } else if (i - 1 < 0) {
+            CheckPoint[i][j][4] = 2;
+        } else if (j + 1 > Col_len) {
+            CheckPoint[i][j][0] = 2;
+        } else if (j - 1 < 0) {
+            CheckPoint[i][j][3] = 2;
+            /////////////////////////////////////////////////////////////////////////
+        } else if (Mapping[i][j] != Mapping[i][j + 1] && Mapping[i][j + 1] != 0) {//check if Diff Color
+            CheckPoint[i][j][0] = 2;
+        } else if (Mapping[i][j] != Mapping[i + 1][j] && Mapping[i + 1][j] != 0) {
+            CheckPoint[i][j][1] = 2;
+        } else if (Mapping[i][j] != Mapping[i][j - 1] && Mapping[i][j - 1] != 0) {
+            CheckPoint[i][j][2] = 2;
+        } else if (Mapping[i][j] != Mapping[i - 1][j] && Mapping[i - 1][j] != 0) {
+            CheckPoint[i][j][3] = 2;
+            //////////////////////////////////////////////////////////////////////////
+        } else if (Mapping[i][j] == Mapping[i][j + 1]) {//check if Same Color
+            CheckPoint[i][j][0] = 1;
+        } else if (Mapping[i][j] != Mapping[i + 1][j]) {
+            CheckPoint[i][j][1] = 1;
+        } else if (Mapping[i][j] != Mapping[i][j - 1]) {
+            CheckPoint[i][j][2] = 1;
+        } else if (Mapping[i][j] != Mapping[i - 1][j]) {
+            CheckPoint[i][j][3] = 1;
+            ///////////////////////////////////////////////////////////////////////////
+        } else if (Mapping[i][j + 1] == 0) {//check if Nothing here
+            CheckPoint[i][j][0] = 0;
+            Mark = 1;
+        } else if (Mapping[i + 1][j] == 0) {
+            CheckPoint[i][j][1] = 0;
+            Mark = 1;
+        } else if (Mapping[i][j - 1] == 0) {
+            CheckPoint[i][j][2] = 0;
+            Mark = 1;
+        } else if (Mapping[i - 1][j] == 0) {
+            CheckPoint[i][j][3] = 0;
+            Mark = 1;
+            ///////////////////////////////////////////////////////////////////////////
         }
+        return Mark;
     }
 }
 
